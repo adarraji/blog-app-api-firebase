aa0e3caad8614de86f3201c72ec80026cbeb49d5
adarraji <a.darraji@outlook.com>
2022-11-01 07:54:39 -0400

    fix, removed test code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const expres = require("express");
+const express = require("express");
 const knex = require("knex");
 require("dotenv").config();
 
@@ -7,18 +7,8 @@ const db = knex({
     connection: process.env.PG_DATABASE_URL,
 });
 
-// db("users").insert({
-//     email: "tes23@gmail.com",
-//     name: "user2",
-//     joined: new Date()
-// }).then(console.log)
-
-db.select("*").from("users").then(data => {
-    console.log(data);
-})
-
-const app = expres();
-app.use(expres.json());
+const app = express();
+app.use(express.json());
 
 
 app.listen(process.env.POST || "3000", () => {
