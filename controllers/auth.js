09827cdf867b39b1467bc36c6e157df1a6b7a84a
adarraji <a.darraji@outlook.com>
2022-11-01 17:19:29 -0400

    UPDATED RESGISTER FUNCTION

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,16 +4,25 @@ const register = async (req, res) => {
 
     const { username, email, password } = req.body;
 
+    // CHECK IF FIELDS ARE EMPTY
     if (!username || !email || !password) {
         return res.status(400).json("Invalid form submission");
     }
 
     try {
+
+        // CHECK EXISING USER
+        const data = await db.select("*").from("users").where("username", "=", username).orWhere("email", "=", email);
+
+        if (data.length) {
+            return res.status(400).json("User already exists");
+        }
+
+        // INSERT USER
         const user = await db("users").insert({ username: username, email: email, password: password }).returning("username");
         res.status(201).json(user);
     }
     catch (err) {
-        console.log(err);
         res.status(400).json("unable to register");
     }
 };
