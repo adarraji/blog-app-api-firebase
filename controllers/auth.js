4896957a6789203fc9af8080ff07c09d780d0639
adarraji <a.darraji@outlook.com>
2022-11-01 15:52:12 -0400

    added register controller to insert to dv

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,21 @@
 const db = require("../db");
 
-const register = (req, res) => {
+const register = async (req, res) => {
 
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json("Invalid form submission");
+    }
+
+    try {
+        const user = await db("users").insert({ username: username, email: email, password: password }).returning("username");
+        res.status(201).json(user);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(400).json("unable to register");
+    }
 };
 
 const login = (req, res) => {
