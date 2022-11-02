14269d5a9d441179f489d68cd0d75f8d7ee5cefd
adarraji <a.darraji@outlook.com>
2022-11-02 14:22:57 -0400

    added posts routes and controllers

---
+++ controllers/post.js
@@ -0,0 +1,24 @@
+const db = require("../db");
+
+const getPosts = (req, res) => {
+
+};
+
+const addPosts = (req, res) => {
+
+};
+
+const updatePosts = (req, res) => {
+
+};
+
+const deletePosts = (req, res) => {
+
+};
+
+module.exports = {
+    getPosts: getPosts,
+    addPosts: addPosts,
+    updatePosts: updatePosts,
+    deletePosts: deletePosts
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,3 +1,12 @@
+
 const router = require("express").Router();
+const { getPosts, addPosts, updatePosts, deletePosts } = require("../controllers/post");
+
+router.get("/", getPosts);
+router.get("/:id", getPosts);
+router.post("/", addPosts);
+router.delete("/:id", updatePosts);
+router.update("/:id", deletePosts);
+
 
 module.exports = router;
