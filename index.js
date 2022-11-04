b6e83e99aacffaf6c45d79026f21ac7c0c6fafac
adarraji <a.darraji@outlook.com>
2022-11-04 18:42:12 -0400

    fix

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 app.post("/api/upload", upload.single("file"), function (req, res) {
-    console.log(req.body);
-    return res.status(200).json("Image has been uploadded");
+    const file = req.file;
+    return res.status(200).json(file.filename);
 })
 
 app.use("/api/auth", authRoute);
