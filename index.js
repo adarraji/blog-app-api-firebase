500dd19e43b500e6b544644ff24633d76d447727
adarraji <a.darraji@outlook.com>
2022-11-04 18:40:10 -0400

    added upload route and  multer config

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const multer = require("multer");
 
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads")
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage })
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+    console.log(req.body);
+    return res.status(200).json("Image has been uploadded");
+})
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
