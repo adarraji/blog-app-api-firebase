140dfd082bb2bec714a122b6954a8b6d6128f96a
adarraji <a.darraji@outlook.com>
2022-11-09 13:48:56 -0500

    fix

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors(corsConfig));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./uploads")
+        cb(null, "./upload")
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + file.originalname)
