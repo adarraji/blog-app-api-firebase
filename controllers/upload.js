68390361cd019fa3d1dcfb35636c2f7a5ef2820a
adarraji <a.darraji@outlook.com>
2022-11-09 19:23:15 -0500

    adde server url to file name

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 const getFileName = (req, res) => {
     try {
         const file = req.file;
-        return res.status(200).json(file.filename);
+        return res.status(200).json(`${process.env.SERVER_URL}/uploads/` + file.filename);
     }
     catch (err) {
         return res.status(500).json("Unable to upload file");
