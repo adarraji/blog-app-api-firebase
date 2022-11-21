518188f92343888455dd594e7d0c23d8484b9c87
adarraji <a.darraji@outlook.com>
2022-11-21 09:37:58 -0500

    fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,6 +68,7 @@ const login = async (req, res) => {
         // RESPONSE WITH COOCKIE AND DATA WITHOUT PASSOWRD          
         res.cookie("access_token", token, {
             httpOnly: true,
+            sameSite: "none"
         }).status(200).json(other);
     }
     catch (err) {
