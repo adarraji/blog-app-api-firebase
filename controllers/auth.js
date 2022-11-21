562c0329fccd54e0667da8a8f38fe5fd3c3dee7b
adarraji <a.darraji@outlook.com>
2022-11-21 10:21:44 -0500

    fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -69,6 +69,7 @@ const login = async (req, res) => {
         res.header('Access-Control-Allow-Credentials', true);
         res.header('Access-Control-Allow-Origin', req.headers.origin);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
         res.cookie("access_token", token, {
             httpOnly: true,
             sameSite: "none",
